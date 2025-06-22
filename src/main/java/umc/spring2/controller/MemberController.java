6e1b219dfbaed6592b98819fb2977a8018ee2d3c
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-06-22 11:22:44 +0900

    feat: 멤버 도메인 생성

--- src/main/java/umc/spring2/controller/MemberController.java
+++ src/main/java/umc/spring2/controller/MemberController.java
@@ -1,4 +1,15 @@
 package umc.spring2.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+
+
 }
