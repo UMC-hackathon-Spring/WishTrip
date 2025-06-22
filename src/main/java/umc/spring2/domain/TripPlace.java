9c79b76b05666715cdf5e419c365910daa64eafd
PADO <sanhobox@gmail.com>
2025-06-22 11:09:15 +0900

    여행 기록 도메인 upload

---
+++ src/main/java/umc/spring2/domain/TripPlace.java
@@ -0,0 +1,38 @@
+package umc.spring2.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import umc.spring2.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TripPlace extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 회원 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 255, nullable = false)
+    private String country;
+
+    @Column(length = 255, nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Boolean completed;
+
+    @Column(columnDefinition = "TEXT")
+    private String imageUrl;
+}
