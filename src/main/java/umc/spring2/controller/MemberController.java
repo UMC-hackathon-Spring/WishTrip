ef3ae27579c531ae444fc14450f76e322b92d4ca
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-06-22 11:53:12 +0900

    멤버 이름 조회 service

--- src/main/java/umc/spring2/controller/MemberController.java
+++ src/main/java/umc/spring2/controller/MemberController.java
@@ -1,9 +1,13 @@
 package umc.spring2.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import umc.spring2.dto.MemberResponseDTO;
+import umc.spring2.service.MemberService;
 
 @Validated
 @RestController
@@ -11,5 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/members")
 public class MemberController {
 
+    private final MemberService memberService;
 
 }
---
+++ src/main/java/umc/spring2/dto/MemberResponseDTO.java
@@ -0,0 +1,11 @@
+package umc.spring2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberResponseDTO {
+
+    private String member_name;
+}
