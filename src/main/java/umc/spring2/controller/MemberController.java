24b3bb3b9207ee2cbdda1e85347e301df69e5446
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-06-22 13:29:08 +0900

    임시커밋

--- src/main/java/umc/spring2/controller/MemberController.java
+++ src/main/java/umc/spring2/controller/MemberController.java
@@ -1,10 +1,13 @@
 package umc.spring2.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import umc.spring2.apiPayload.ApiResponse;
 import umc.spring2.convert.MemberConverter;
 import umc.spring2.domain.Member;
 import umc.spring2.dto.MemberRequestDTO;
@@ -17,4 +20,5 @@ import umc.spring2.service.MemberService;
 @RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
+
 }
